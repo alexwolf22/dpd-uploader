11bec5b8b67f45e3a65e5dd6520bf6a0913aa798
alexwolf22 <alex.r.wolf22@gmail.com>
2020-12-17 14:06:42 -0600

    run buid for component

--- dpd_uploader/Uploader.py
+++ dpd_uploader/Uploader.py
@@ -5,23 +5,41 @@ from dash.development.base_component import Component, _explicitize_args
 
 class Uploader(Component):
     """An Uploader component.
-ExampleComponent is an example component.
-It takes a property, `label`, and
-displays it.
-It renders an input with the property `value`
-which is editable by the user.
+
 
 Keyword arguments:
-- id (string; optional): The ID used to identify this component in Dash callbacks.
-- label (string; required): A label that will be printed when this component is rendered.
-- value (string; optional): The value displayed in the input."""
+- maxFiles (number; default 1): Maximum number of files that can be uploaded in one session
+- maxFileSize (number; default 1024 * 1024 * 10): Maximum size per file in bytes.
+- chunkSize (number; default 1024 * 1024): Size of file chunks to send to server.
+- simultaneousUploads (number; optional): Number of simultaneous uploads to select
+- service (string; default '/API/dash-uploader'): The service to send the files to
+- className (string; default 'resumable-default'): Class to add to the upload component by default
+- hoveredClass (string; default 'resumable-hovered'): Class to add to the upload component when it is hovered
+- disabledClass (string; default 'resumable-disabled'): Class to add to the upload component when it is disabled
+- pausedClass (string; default 'resumable-paused'): Class to add to the upload component when it is paused
+- completeClass (string; default 'resumable-complete'): Class to add to the upload component when it is complete
+- uploadingClass (string; default 'resumable-uploading'): Class to add to the upload component when it is uploading
+- defaultStyle (dict; optional): Style attributes to add to the upload component
+- uploadingStyle (dict; optional): Style when upload is in progress
+- completeStyle (dict; optional): Style when upload is completed (upload finished)
+- textLabel (string; default 'Click Here to Select a File'): The string to display in the upload component
+- completedMessage (string; default 'Complete! '): Message to display when upload completed
+- fileNames (list of strings; optional): The names of the files uploaded
+- filetypes (list of strings; default undefined): List of allowed file types, e.g. ['jpg', 'png']
+- startButton (boolean; default True): Whether or not to have a start button
+- pauseButton (boolean; default True): Whether or not to have a pause button
+- cancelButton (boolean; default True): Whether or not to have a cancel button
+- disableDragAndDrop (boolean; default False): Whether or not to allow file drag and drop
+- id (string; default 'default-dash-uploader-id'): User supplied id of this component
+- isCompleted (boolean; default False): The boolean flag telling if upload is completed.
+- upload_id (string; default ''): The ID for the upload event (for example, session ID)"""
     @_explicitize_args
-    def __init__(self, id=Component.UNDEFINED, label=Component.REQUIRED, value=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['id', 'label', 'value']
+    def __init__(self, maxFiles=Component.UNDEFINED, maxFileSize=Component.UNDEFINED, chunkSize=Component.UNDEFINED, simultaneousUploads=Component.UNDEFINED, service=Component.UNDEFINED, className=Component.UNDEFINED, hoveredClass=Component.UNDEFINED, disabledClass=Component.UNDEFINED, pausedClass=Component.UNDEFINED, completeClass=Component.UNDEFINED, uploadingClass=Component.UNDEFINED, defaultStyle=Component.UNDEFINED, uploadingStyle=Component.UNDEFINED, completeStyle=Component.UNDEFINED, textLabel=Component.UNDEFINED, completedMessage=Component.UNDEFINED, fileNames=Component.UNDEFINED, filetypes=Component.UNDEFINED, startButton=Component.UNDEFINED, pauseButton=Component.UNDEFINED, cancelButton=Component.UNDEFINED, disableDragAndDrop=Component.UNDEFINED, id=Component.UNDEFINED, isCompleted=Component.UNDEFINED, upload_id=Component.UNDEFINED, simultaneuosUploads=Component.UNDEFINED, onUploadErrorCallback=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['maxFiles', 'maxFileSize', 'chunkSize', 'simultaneousUploads', 'service', 'className', 'hoveredClass', 'disabledClass', 'pausedClass', 'completeClass', 'uploadingClass', 'defaultStyle', 'uploadingStyle', 'completeStyle', 'textLabel', 'completedMessage', 'fileNames', 'filetypes', 'startButton', 'pauseButton', 'cancelButton', 'disableDragAndDrop', 'id', 'isCompleted', 'upload_id']
         self._type = 'Uploader'
         self._namespace = 'dpd_uploader'
         self._valid_wildcard_attributes =            []
-        self.available_properties = ['id', 'label', 'value']
+        self.available_properties = ['maxFiles', 'maxFileSize', 'chunkSize', 'simultaneousUploads', 'service', 'className', 'hoveredClass', 'disabledClass', 'pausedClass', 'completeClass', 'uploadingClass', 'defaultStyle', 'uploadingStyle', 'completeStyle', 'textLabel', 'completedMessage', 'fileNames', 'filetypes', 'startButton', 'pauseButton', 'cancelButton', 'disableDragAndDrop', 'id', 'isCompleted', 'upload_id']
         self.available_wildcard_properties =            []
 
         _explicit_args = kwargs.pop('_explicit_args')
@@ -29,7 +47,7 @@ Keyword arguments:
         _locals.update(kwargs)  # For wildcard attrs
         args = {k: _locals[k] for k in _explicit_args if k != 'children'}
 
-        for k in ['label']:
+        for k in []:
             if k not in args:
                 raise TypeError(
                     'Required argument `' + k + '` was not specified.')
